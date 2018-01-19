2d8103eb9c91c6fc54049972bc97242ea1b84b85
yangwch <fire_snow@163.com>
2018-01-19 14:56:44 +0800

    页面细节

--- src/apps/plugin/menu/index.vue
+++ src/apps/plugin/menu/index.vue
@@ -1,19 +1,45 @@
 <template>
-  <el-menu :default-active="$route.path" router class="sub-menus">
-    <submenu :data="item" v-for="(item, index) in data" :key="item.id"></submenu>
+  <el-menu :default-active="$route.path" router class="yt-menu">
+    <submenu :data="item" v-for="(item, index) in data" :key="index"></submenu>
   </el-menu>
 </template>
+<style lang="less">
+  .yt-menu {
+    height: 100%;
+    .el-menu {
+      border-right: 0;
+    }
+    i {
+      margin-right: 5px;
+      font-size: 16px;
+    }
+  }
+</style>
+
 <script>
   import submenu from './submenu'
   export default {
+    name: 'ytMenu',
     components: {
       submenu
     },
     props: {
+      /**
+       * 菜单数组数据
+       */
       data: {
         type: Array,
         default () {
           return []
+        }
+      },
+      /**
+       * 是否收起
+       */
+      collapse: {
+        type: Boolean,
+        default () {
+          return false
         }
       }
     }
--- src/apps/plugin/menu/submenu.vue
+++ src/apps/plugin/menu/submenu.vue
@@ -1,7 +1,7 @@
 <template>
-  <el-menu-item v-if="data.children == null || data.children.length == 0" :index="data.attributes.url" class="custom-submenu"><i :class="data.icon || ''"></i>{{data.text}}</el-menu-item>
+  <el-menu-item v-if="data.children == null || data.children.length == 0" :index="data.attributes.url" class="custom-submenu"><i :class="data.attributes.icon || ''"></i><span slot="title">{{data.text}}</span></el-menu-item>
   <el-submenu v-else :index="data.id.toString()" class="custom-submenu">
-    <template slot="title"><i :class="data.icon || ''"></i><span v-text="data.text"></span></template>
+    <template slot="title"><i :class="data.attributes.icon || ''"></i><span v-text="data.text"></span></template>
     <yt-submenu :data="item" v-for="(item, index) in data.children" :key="index"></yt-submenu>
   </el-submenu>
 </template>
